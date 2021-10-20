2da898bb842bfc6872031bf1c91bf6c2dbdccbf4
Artur <arturrsilveira85@gmail.com>
2021-10-19 21:27:42 -0300

    Principio S e D de SOLID aplicado na Rota de Criação de Categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
-import { response, Router } from 'express';
+import { Router } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -10,13 +11,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
     
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-    if (categoryAlreadyExists) {
-        return response.status(400).json({error: 'Category already exists'});
-    }
-
-    categoriesRepository.create({name, description});
+    createCategoryService.execute({name, description});
 
     return response.status(201).send();
 });
