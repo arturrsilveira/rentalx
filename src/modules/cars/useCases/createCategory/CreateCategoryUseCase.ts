4572e3017a648bd4e1aaeb9a5f30d83760a96e04
Artur <arturrsilveira85@gmail.com>
2021-10-30 15:40:42 -0300

    Rota de criação de especificações refatorada

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,4 +1,3 @@
-import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { ICategoriesRepository } from "../../repositories/IcategoriesRepository";
 
 interface IRequest {
